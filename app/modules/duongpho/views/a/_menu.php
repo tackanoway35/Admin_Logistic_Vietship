d838c551fbfca5b00af0ee368a4124dc1f034b5e
tte <tackanoway35@gmail.com>
2017-09-13 11:34:00 +0700

    Xong phan them xoa sua nhan vien

--- app/modules/duongpho/views/a/_menu.php
+++ app/modules/duongpho/views/a/_menu.php
@@ -11,9 +11,9 @@ $module = $this->context->module->id;
             <?php if($action == 'edit' || $action == 'photos') : ?>
                 <i class="glyphicon glyphicon-chevron-left font-12"></i>
             <?php endif; ?>
-            <?= "Danh sách đường phố" ?>
+            Danh sách đường phố
         </a>
     </li>
-    <li <?= ($action === 'create') ? 'class="active"' : '' ?>><a href="<?= Url::to(['/admin/'.$module.'/a/create']) ?>"><?= "Tạo đường phố mới" ?></a></li>
+    <li <?= ($action === 'create') ? 'class="active"' : '' ?>><a href="<?= Url::to(['/admin/'.$module.'/a/create']) ?>">Tạo đường phố mới</a></li>
 </ul>
 <br/>
